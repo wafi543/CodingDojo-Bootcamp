7e030c5d719fb2c3a1fada17de9909808a0fa7ab
Wafi AlShammari <wafi543@outlook.sa>
2019-10-03 12:07:58 +0300

    Update app.js

--- MEAN_Stack/Node/landing_page/app.js
+++ MEAN_Stack/Node/landing_page/app.js
@@ -1,22 +1,22 @@
 const http = require('http');
 const fs = require('fs');
-const server = http.createServer(function (request, response) {
+const server = http.createServer((request, response) => {
     if(request.url === '/') {
-        fs.readFile('index.html', 'utf8', function (errors, contents) {
+        fs.readFile('index.html', 'utf8', (errors, contents) => {
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(contents); 
             response.end();
         });
     }
     else if (request.url === "/ninjas") {
-         fs.readFile('ninjas.html', 'utf8', function (errors, contents) {
+         fs.readFile('ninjas.html', 'utf8', (errors, contents) => {
              response.writeHead(200, {'Content-type': 'text/html'});
              response.write(contents); 
              response.end();
          });
     }
     else if (request.url === "/dojos/new") {
-        fs.readFile('dojos.html', 'utf8', function (errors, contents) {
+        fs.readFile('dojos.html', 'utf8', (errors, contents) => {
             response.writeHead(200, {'Content-type': 'text/html'});
             response.write(contents); 
             response.end();
