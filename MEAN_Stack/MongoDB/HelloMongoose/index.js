02cba7893b830227c38956cc59f3b1575ee3fe69
Wafi AlShammari <wafi543@outlook.sa>
2019-10-14 23:14:58 +0300

    qouting_dojo

--- MEAN_Stack/MongoDB/HelloMongoose/index.js
+++ MEAN_Stack/MongoDB/HelloMongoose/index.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({extended: true}));
 app.listen(8000, () => console.log("listening on port 8000"));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+mongoose.connect('mongodb://localhost/users_db', {useNewUrlParser: true});
+const UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+const User = mongoose.model('User', UserSchema);
 app.get('/', (req, res) => {
-    res.render('index')
- });
+    User.find()
+        .then(data => res.render("index", {users: data}))
+        .catch(err => res.json(err));
+});
 app.post("/users", (req, res) => {
-    mongoose.connect('mongodb://localhost/name_of_your_DB', {useNewUrlParser: true});
-})
+    const user = new User();
+    console.log(req.body)
+    user.name = req.body.name;
+    user.age = req.body.age;
+    user.save()
+      .then(newUserData => console.log('user created: ', newUserData))
+      .catch(err => console.log(err));
+     
+    res.redirect('/');
+ })
