d163589f6029926c6f0d6c4441400c9dfe66e127
Wafi AlShammari <wafi543@outlook.sa>
2019-10-19 18:43:38 +0300

    hello_angular

--- MEAN_Stack/MongoDB/Restful_task/server.js
+++ MEAN_Stack/MongoDB/Restful_task/server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended: true}));
 mongoose.connect('mongodb://localhost/RestfulTasks_db', { useNewUrlParser: true });
-app.listen(8000, () => console.log("listening on port 8000"));
+app.use(express.static( __dirname + '/public/dist/public' ));
 
+app.listen(8000, () => console.log("listening on port 8000"));
 const TaskSchema = new mongoose.Schema({
     title: String,
     description: {type: String, default: ''},
