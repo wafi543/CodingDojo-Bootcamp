a48db97f885750e05ff673e9e8c9fba5660ebaff
Wafi AlShammari <wafi543@outlook.sa>
2019-10-20 17:40:25 +0300

    angular-fetch-data

--- MEAN_Stack/MongoDB/Restful_task/public/src/app/app.module.ts
+++ MEAN_Stack/MongoDB/Restful_task/public/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
+import {HttpService} from './http.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
